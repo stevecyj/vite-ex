fbc9e80d6f119f4c28a90ed80c6313843e9dcf31
root <root@localhost.localdomain>
2022-03-24 13:55:20 +0800

    use antdv in vite

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>Homehahahahahahah, jojojojo</div>
+  <div>Homehahahahahahah, jojojojo, hahahahah</div>
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <a-button type="primary">Primary Button</a-button>
+  <a-button>Default Button</a-button>
+  <a-button type="dashed">Dashed Button</a-button>
+  <a-button type="text">Text Button</a-button>
+  <a-button type="link">Link Button</a-button>
 </template>
 <script>
 export default {
