0a4873520ad6f01ede61105c7c914e5ec431021a
root <root@localhost.localdomain>
2022-03-29 20:19:23 +0800

    table,undone

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,53 @@
+<script>
+export default {
+    name: 'HomeView',
+    setup() {
+        return {
+            dataSource: [
+                {
+                    key: '1',
+                    name: '胡彦斌',
+                    age: 32,
+                    address: '西湖区湖底公园1号',
+                    quarter: 'Q1',
+                    EPS2019: '2',
+                },
+                {
+                    key: '2',
+                    name: '胡彦祖',
+                    age: 42,
+                    address: '西湖区湖底公园1号',
+                    quarter: 'Q2',
+                    EPS2019: '2',
+                },
+            ],
+
+            columns: [
+                {
+                    title: '季度',
+                    dataIndex: 'quarter',
+                    key: 'quarter',
+                },
+                {
+                    title: '2019 EPS',
+                    dataIndex: 'EPS2019',
+                    key: 'EPS2019',
+                },
+                {
+                    title: '2021 EPS',
+                    dataIndex: 'address',
+                    key: 'address',
+                },
+                {
+                    title: '2022 EPS',
+                    dataIndex: 'address',
+                    key: 'address',
+                },
+            ],
+        }
+    },
+}
+</script>
 <template>
   <div>Homehahahahahahah, jojojojo, hahahahah</div>
   <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
@@ -6,9 +56,9 @@
   <a-button type="dashed">Dashed Button</a-button>
   <a-button type="text">Text Button</a-button>
   <a-button type="link">Link Button</a-button>
+  <a-divider />
+  <a-table
+    :dataSource="dataSource"
+    :columns="columns"
+  />
 </template>
-<script>
-export default {
-    name: 'HomeView',
-}
-</script>
